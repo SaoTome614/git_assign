a1ca9c020e324c8bc7934baeb0486d45fe83d126
tomala <tomalaroch2002@gmail.com>
2024-06-24 10:24:29 +0900

    Base of the program created

---
+++ pattern2.py
@@ -0,0 +1,30 @@
+import random
+
+def number_guessing():
+
+    answer = random.randint(100, 999)
+    attempts = 0
+    max_attempts = 10
+
+    print("3桁の数字を入力してください")
+
+    while attempts < max_attempts:
+        guess = int(input(""))
+        if not (100 <= guess < 999):
+            print("【3桁】の数字を入力してください")
+            continue
+
+        if guess < answer:
+            print("もっと大きいです")
+        elif guess > answer:
+            print("もっと小さいです")
+        else:
+            print("正解!")
+            break
+
+        attempts += 1
+    
+    else:
+        print("残念!")
+
+number_guessing()
